3306a76000ec0d056790acbab67fe6b70580a88f
seonghun <seonghunkim468@gmail.com>
2021-09-28 19:45:19 +0900

    make heroku

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.listen(port);
+
+app.use('/api/data', (req, res) => {
+  res.json({ greeting: 'Hello World!' });
+});
+console.log(`server running at http ${port}`);
