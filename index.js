e11d2745e2d5a55b9197292d5a1d26c39ff3512c
seonghun <seonghunkim468@gmail.com>
2021-09-28 23:19:32 +0900

    fix deploy error

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
-
-app.listen(port);
+const path = require('path');
 
 app.use('/api/data', (req, res) => {
   res.json({ greeting: 'Hello World!' });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 console.log(`server running at http ${port}`);
